a9cb3bc5b88645fc11af87857285a78ee370ed2e
Sumon Ahmed <bdsumon4u@gmail.com>
2024-12-15 14:27:14 +0600

    wip

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\ComposerServiceProvider::class,
+    App\Providers\BladeServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     Yajra\DataTables\DataTablesServiceProvider::class,
 ];
