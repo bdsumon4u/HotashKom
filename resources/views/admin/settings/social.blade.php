909e00519f8c1dccc186cf007905f473789a6b60
Sumon Ahmed <bdsumon4u@gmail.com>
2024-12-15 21:32:22 +0600

    wip

--- resources/views/admin/settings/social.blade.php
+++ resources/views/admin/settings/social.blade.php
@@ -1,7 +1,7 @@
 <div class="tab-pane active" id="item-2" role="tabpanel">
     <div class="row">
         <div class="col-sm-12">
-            <h4><small class="border-bottom mb-1">Social</small></h4>
+            <h4><small class="mb-1 border-bottom">Social</small></h4>
         </div>
     </div>
 
@@ -36,6 +36,18 @@
                     <span class="input-group-text"><i class="fa fa-youtube"></i></span>
                 </div>
                 <x-input type="url" name="social[youtube][link]" :value="$social->youtube->link ?? ''" />
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16">
+                        <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
+                        </svg>
+                    </span>
+                </div>
+                <x-input type="url" name="social[tiktok][link]" :value="$social->tiktok->link ?? ''" />
             </div>
         </div>
     </div>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -46,7 +46,7 @@
                                 @foreach($social ?? [] as $item => $data)
                                     @if(($link = $data->link ?? false) && $link != '#')
                                     <li class="footer-newsletter__social-link footer-newsletter__social-link--{{ $item }}">
-                                        <a href="{{ url($link ?? '#') }}" target="_blank">
+                                        <a href="{{ url($link ?? '#') }}" target="_blank" @if($item == 'tiktok') style="background-color: black" @endif>
                                             @switch($item)
                                                 @case('facebook')
                                                 <i class="fa fab fa-facebook-f"></i>
@@ -59,6 +59,11 @@
                                                 @break
                                                 @case('youtube')
                                                 <i class="fa fab fa-youtube"></i>
+                                                @break
+                                                @case('tiktok')
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16">
+                                                <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
+                                                </svg>
                                                 @break
                                             @endswitch
                                         </a>
@@ -74,7 +79,7 @@
                 <div class="site-footer__copyright">
                     Copyright 2020 - {{ date('Y') }} &copy; {{ $company->name ?? '' }}
                 </div>
-                <div class="site-footer__payments">
+                <div class="site-footer__payments d-none">
                     Developed By <a href="{{$company->dev_link??'https://cyber32.com'}}" class="text-danger">{{$company->dev_name??'Cyber 32'}}</a>
                 </div>
             </div>
