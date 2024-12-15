8caaba3c3e3bab2ece19f377ad71422e4e10acda
Sumon Ahmed <bdsumon4u@gmail.com>
2024-12-15 14:52:39 +0600

    wip

--- app/Providers/BladeServiceProvider.php
+++ app/Providers/BladeServiceProvider.php
@@ -24,38 +24,41 @@ class BladeServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::directive('exp', function ($expression) {
-            return "<?php $expression ?>";
+        // Refactor the @exp directive
+        Blade::extend(function ($view) {
+            return preg_replace('/@exp\((.*?)\)/', '<?php $1 ?>', $view);
         });
 
+        // Refactor the @title and @content directives
         foreach (['title', 'content'] as $layout) {
-            Blade::directive($layout, function ($expression) use ($layout) {
-                return "<?php \$__env->startSection('{$layout}', {$expression}); ?>";
-            });
-            Blade::directive("end{$layout}", function () {
-                return '<?php $__env->stopSection(); ?>';
+            Blade::extend(function ($view) use ($layout) {
+                $startPattern = "/@$layout\((.*?)\)/";
+                $startReplacement = "<?php \$__env->startSection('{$layout}', $1); ?>";
+
+                $endPattern = "/@end{$layout}/";
+                $endReplacement = '<?php $__env->stopSection(); ?>';
+
+                $view = preg_replace($startPattern, $startReplacement, $view);
+                return preg_replace($endPattern, $endReplacement, $view);
             });
         }
 
-        foreach (['title', 'content'] as $layout) {
-            Blade::directive($layout, function ($expression) use ($layout) {
-                return "<?php \$__env->startSection('{$layout}', {$expression}); ?>";
-            });
-            Blade::directive("end{$layout}", function () {
-                return '<?php $__env->stopSection(); ?>';
-            });
-        }
+        // Refactor the @errors directive
+        Blade::extend(function ($view) {
+            $pattern = '/@errors/';
+            $replacement = <<<'EOT'
+<?php if ($errors->any()): ?>
+    <div class="alert alert-danger" role="alert">
+        <ul>
+            <?php foreach($errors->all() as $error): ?>
+            <li>{{ $error }}</li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+<?php endif ?>
+EOT;
 
-        Blade::directive('errors', function () {
-            return '<?php if ($errors->any()): ?>
-                <div class="alert alert-danger" role="alert">
-                    <ul>
-                        <?php foreach($errors->all() as $error): ?>
-                        <li>{{ $error }}</li>
-                        <?php endforeach ?>
-                    </ul>
-                </div>
-            <?php endif ?>';
+            return preg_replace($pattern, $replacement, $view);
         });
     }
 }
