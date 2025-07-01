7c9c791cd29272f52f83d6b71d475e8ef3c1c1f5
Sumon Ahmed <bdsumon4u@gmail.com>
2025-07-01 15:41:15 +0600

    fix: comment out note fields in OrderController to prevent potential data leakage

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -281,7 +281,7 @@ class OrderController extends Controller
             'recipient_address' => $order->address ?? 'N/A',
             'recipient_phone' => $order->phone ?? '',
             'cod_amount' => intval($order->data['shipping_cost']) + intval($order->data['subtotal']) - intval($order->data['advanced'] ?? 0) - intval($order->data['discount'] ?? 0),
-            'note' => $order->note,
+            // 'note' => $order->note,
         ])->toJson();
 
         $response = Http::withHeaders([
@@ -325,7 +325,7 @@ class OrderController extends Controller
             // "recipient_area"      => "", // Find in Area method
             'delivery_type' => 48, // 48 for normal delivery or 12 for on demand delivery
             'item_type' => 2, // 1 for document, 2 for parcel
-            'special_instruction' => $order->note,
+            // 'special_instruction' => $order->note,
             'item_quantity' => 1, // item quantity
             'item_weight' => $order->data['weight'] ?? 0.5, // parcel weight
             'amount_to_collect' => intval($order->data['shipping_cost']) + intval($order->data['subtotal']) - intval($order->data['advanced'] ?? 0) - intval($order->data['discount'] ?? 0), // - $order->deliveryCharge, // amount to collect
@@ -356,7 +356,7 @@ class OrderController extends Controller
             // "customer_area"      => "", // Find in Area method
             // 'delivery_type' => 48, // 48 for normal delivery or 12 for on demand delivery
             // 'item_type' => 2, // 1 for document, 2 for parcel
-            'instruction' => $order->note,
+            // 'instruction' => $order->note,
             'is_closed_box' => false,
             'value' => 100,
             // 'item_quantity' => 1, // item quantity
