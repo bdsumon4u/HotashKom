b345990ace5222aa7dd79ff34c140381ba456e1c
Sumon Ahmed <bdsumon4u@gmail.com>
2025-08-08 22:07:18 +0600

    feat: update authentication indicator to conditionally display login link for users and improve UI with SVG icon

--- resources/views/partials/auth-indicator.blade.php
+++ resources/views/partials/auth-indicator.blade.php
@@ -1,4 +1,18 @@
-@guest('user')
+@if ($show_option->customer_login ?? isOninda())
+<div class="ml-2 indicator">
+    <a href="{{ auth('user')->check() ? route('reseller.dashboard') : route('user.login') }}" class="indicator__button">
+        <span class="indicator__area">
+            <svg width="20" height="20">
+                <path
+                    d="M13.7,10.7C15.1,9.6,16,7.9,16,6c0-3.3-2.7-6-6-6S4,2.7,4,6c0,1.9,0.9,3.6,2.3,4.7C2.6,12.2,0,15.8,0,20h2c0-4.4,3.6-8,8-8 s8,3.6,8,8h2C20,15.8,17.4,12.2,13.7,10.7z M6,6c0-2.2,1.8-4,4-4s4,1.8,4,4c0,2.2-1.8,4-4,4S6,8.2,6,6z">
+                </path>
+            </svg>
+        </span>
+    </a>
+</div>
+@endif
+
+{{-- @guest('user')
     @if ($show_option->customer_login ?? isOninda())
         <div class="ml-2 indicator">
             <a href="{{ auth('admin')->check() ? route('admin.home') : route('user.login') }}" class="indicator__button">
@@ -53,4 +67,4 @@
             </div>
         </div>
     </div>
-@endguest
+@endguest --}}
