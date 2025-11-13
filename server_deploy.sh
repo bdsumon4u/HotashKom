60a92883857220de7648e9b398b981c2c1e05827
Sumon Ahmed <bdsumon4u@gmail.com>
2025-11-13 14:10:32 +0600

    Refactor server deployment script to remove the --dev branch argument and streamline the git pull command. Update composer install logic to conditionally include --no-dev based on input argument. Change cache clearing command to optimize:clear for improved performance.

--- server_deploy.sh
+++ server_deploy.sh
@@ -3,23 +3,16 @@ set -e
 
 echo "Deploying application ..."
 
-# Check if --dev argument is passed
-if [ "$1" = "--dev" ]; then
-    branch="dev"
-else
-    branch="master"
-fi
-
 # Enter maintenance mode
 (/opt/alt/php83/usr/bin/php artisan down) || true
     # Update codebase
     # git fetch origin production
     # git reset --hard origin/production
-    git pull origin $branch --force
+    git pull origin master --force
 
     /opt/alt/php83/usr/bin/php "$([ -f "./composer.phar" ] && echo "./composer.phar" || command -v composer || echo /opt/cpanel/composer/bin/composer)" install \
         --no-interaction --prefer-dist --optimize-autoloader --no-progress \
-        $(if [ "$branch" != "dev" ]; then echo "--no-dev"; fi)
+        $(if [ "$1" = "--no-dev" ]; then echo "--no-dev"; fi)
 
     # Ensure all tables use InnoDB before running migrations
     /opt/alt/php83/usr/bin/php artisan db:convert-innodb || true
@@ -31,7 +24,7 @@ fi
     # ...
 
     # Clear cache
-    /opt/alt/php83/usr/bin/php artisan optimize
+    /opt/alt/php83/usr/bin/php artisan optimize:clear
 
     # Reload PHP to update opcache
     # echo "" | sudo -S service php7.4-fpm reload
