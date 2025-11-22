dfb560594f7d24d848f1b4445b5af7bfd9949e90
Sumon Ahmed <bdsumon4u@gmail.com>
2025-11-22 17:04:22 +0600

    Fix typo in order completion message and remove unused middleware from app bootstrap.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,10 +22,6 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-            \Spatie\ResponseCache\Middlewares\CacheResponse::class,
-        ]);
-
         $middleware->alias([
             'auth' => Authenticate::class,
             'guest' => RedirectIfAuthenticated::class,
