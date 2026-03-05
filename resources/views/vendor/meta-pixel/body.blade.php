3dd5e65c54a5ceafa9cdb61e4abe496c8b9afa94
Sumon Ahmed <bdsumon4u@gmail.com>
2026-03-05 12:51:24 +0600

    style: improve code formatting and spacing in Meta Pixel integration

--- resources/views/vendor/meta-pixel/body.blade.php
+++ resources/views/vendor/meta-pixel/body.blade.php
@@ -1,5 +1,5 @@
-@if($metaPixel->isEnabled())
-    @unless(empty($eventLayer))
+@if ($metaPixel->isEnabled())
+    @unless (empty($eventLayer))
         <!-- Meta Pixel Events -->
         <script id="fb-pixel-standard-events" data-fb-pixel-tracking="standard">
             (function() {
@@ -7,7 +7,7 @@
                     window.__fbPixelTrackedEvents = new Set();
                 }
 
-                @foreach($eventLayer as $eventName => $metaPixel)
+                @foreach ($eventLayer as $eventName => $metaPixel)
                     (function() {
                         const eventName = '{{ $eventName }}';
                         const eventKey = 'fbq_{{ hash('sha256', json_encode($eventLayer)) }}_' + eventName;
@@ -17,12 +17,14 @@
                         }
                         window.__fbPixelTrackedEvents.add(eventKey);
 
-                        @if(empty($metaPixel['event_id']) && empty($metaPixel['data']))
+                        @if (empty($metaPixel['event_id']) && empty($metaPixel['data']))
                             fbq('track', eventName);
-                        @elseif(empty($metaPixel['event_id']))
+                        @elseif (empty($metaPixel['event_id']))
                             fbq('track', eventName, {{ Js::from($metaPixel['data']) }});
                         @else
-                            fbq('track', eventName, {{ Js::from($metaPixel['data']) }}, {eventID: '{{ $metaPixel['event_id'] }}'});
+                            fbq('track', eventName, {{ Js::from($metaPixel['data']) }}, {
+                                eventID: '{{ $metaPixel['event_id'] }}'
+                            });
                         @endif
                     })();
                 @endforeach
@@ -31,7 +33,7 @@
         <!-- End Meta Pixel Events -->
     @endunless
 
-    @unless(empty($customEventLayer))
+    @unless (empty($customEventLayer))
         <!-- Meta Pixel Custom Events -->
         <script id="fb-pixel-custom-events" data-fb-pixel-tracking="custom">
             (function() {
@@ -39,7 +41,7 @@
                     window.__fbPixelTrackedEvents = new Set();
                 }
 
-                @foreach($customEventLayer as $customEventName => $metaPixel)
+                @foreach ($customEventLayer as $customEventName => $metaPixel)
                     (function() {
                         const eventName = '{{ $customEventName }}';
                         const eventKey = 'fbq_custom_{{ hash('sha256', json_encode($customEventLayer)) }}_' + eventName;
@@ -49,12 +51,14 @@
                         }
                         window.__fbPixelTrackedEvents.add(eventKey);
 
-                        @if(empty($metaPixel['event_id']) && empty($metaPixel['data']))
+                        @if (empty($metaPixel['event_id']) && empty($metaPixel['data']))
                             fbq('trackCustom', eventName);
-                        @elseif(empty($metaPixel['event_id']))
+                        @elseif (empty($metaPixel['event_id']))
                             fbq('trackCustom', eventName, {{ Js::from($metaPixel['data']) }});
                         @else
-                            fbq('trackCustom', eventName, {{ Js::from($metaPixel['data']) }}, {eventID: '{{ $metaPixel['event_id'] }}'});
+                            fbq('trackCustom', eventName, {{ Js::from($metaPixel['data']) }}, {
+                                eventID: '{{ $metaPixel['event_id'] }}'
+                            });
                         @endif
                     })();
                 @endforeach
