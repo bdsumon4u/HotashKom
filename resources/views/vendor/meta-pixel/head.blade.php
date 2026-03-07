f2a2ac77d134dd0d13712b23bdbbac3fcb74ad87
Sumon Ahmed <bdsumon4u@gmail.com>
2026-03-07 15:04:11 +0600

    Revert "style: improve code formatting and spacing in Meta Pixel integration"
    This reverts commit 3dd5e65c54a5ceafa9cdb61e4abe496c8b9afa94.

--- resources/views/vendor/meta-pixel/head.blade.php
+++ resources/views/vendor/meta-pixel/head.blade.php
@@ -1,46 +1,30 @@
-@if ($metaPixel->isEnabled())
+@if($metaPixel->isEnabled())
     <!-- Meta Pixel Code -->
     <script id="fb-pixel-init" data-fb-pixel-tracking="init">
         (function() {
             if (!window.__fbPixelInitialized) {
-                ! function(f, b, e, v, n, t, s) {
-                    if (f.fbq) return;
-                    n = f.fbq = function() {
-                        n.callMethod ?
-                            n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-                    };
-                    if (!f._fbq) f._fbq = n;
-                    n.push = n;
-                    n.loaded = !0;
-                    n.version = '2.0';
-                    n.queue = [];
-                    t = b.createElement(e);
-                    t.async = !0;
-                    t.src = v;
-                    s = b.getElementsByTagName(e)[0];
-                    s.parentNode.insertBefore(t, s)
-                }(window, document, 'script',
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
                     'https://connect.facebook.net/en_US/fbevents.js');
 
-                @if (false)
-                    @if ($user = $metaPixel->getUser())
-                        @if ($userIdAsString)
-                            fbq('init', '{{ $metaPixel->pixelId() }}', {
-                                em: '{{ $user['em'] }}',
-                                external_id: '{{ $user['external_id'] }}'
-                            });
+                @if(false)
+                    @if($user = $metaPixel->getUser())
+                        @if($userIdAsString)
+                            fbq('init', '{{ $metaPixel->pixelId() }}', {em: '{{ $user['em'] }}', external_id: '{{ $user['external_id'] }}'});
                         @else
-                            fbq('init', '{{ $metaPixel->pixelId() }}', {
-                                em: '{{ $user['em'] }}',
-                                external_id: {{ $user['external_id'] }}
-                            });
+                            fbq('init', '{{ $metaPixel->pixelId() }}', {em: '{{ $user['em'] }}', external_id: {{ $user['external_id'] }}});
                         @endif
                     @else
                         fbq('init', '{{ $metaPixel->pixelId() }}');
                     @endif
                 @else
-                    @foreach (explode(' ', $metaPixel->pixelId()) as $id)
-                        @if ($id)
+                    @foreach(explode(' ', $metaPixel->pixelId()) as $id)
+                        @if($id)
                             fbq('init', '{{ $id }}');
                         @endif
                     @endforeach
@@ -53,8 +37,7 @@
         })();
     </script>
     <noscript>
-        <img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id={{ $metaPixel->pixelId() }}&ev=PageView&noscript=1" />
+        <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id={{ $metaPixel->pixelId() }}&ev=PageView&noscript=1"/>
     </noscript>
     <!-- End Meta Pixel Code -->
 @endif
