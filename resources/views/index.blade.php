db320c7393d91b428f2e090c6a70d2cdddda4812
Sumon Ahmed <bdsumon4u@gmail.com>
2026-06-06 19:52:33 +0600

    feat: relocate brand carousel section in index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -130,6 +130,59 @@
     </div>
 </div>
 @endif
+
+@if(($show_option = setting('show_option'))->brand_carousel ?? false)
+<div class="block block-products-carousel" data-layout="grid-cat">
+    <div class="container">
+        <div class="block-header">
+            <h3 class="block-header__title" style="padding: 0.375rem 1rem;">
+                <a href="{{ route('brands') }}" wire:navigate.hover>Brands</a>
+            </h3>
+            <div class="block-header__divider"></div>
+            <div class="block-header__arrows-list">
+                <button class="block-header__arrow block-header__arrow--left" type="button">
+                    <svg width="7px" height="11px">
+                        <use xlink:href="{{ asset('strokya/images/sprite.svg#arrow-rounded-left-7x11') }}"></use>
+                    </svg>
+                </button>
+                <button class="block-header__arrow block-header__arrow--right" type="button">
+                    <svg width="7px" height="11px">
+                        <use xlink:href="{{ asset('strokya/images/sprite.svg#arrow-rounded-right-7x11') }}"></use>
+                    </svg>
+                </button>
+            </div>
+        </div>
+        <div class="block-products-carousel__slider">
+            <div class="block-products-carousel__preloader"></div>
+            <div class="owl-carousel">
+                @foreach(brands()->chunk(1) as $brands)
+                <div>
+                    @foreach($brands as $brand)
+                    <div class="products-list__item">
+                        <div class="product-card">
+                            <div class="product-card__image">
+                                <a href="{{ route('brands.products', $brand) }}" wire:navigate.hover>
+                                    <img src="{{ cdn($brand->image_src, 100, 100) }}" alt="Product Image">
+                                </a>
+                            </div>
+                            <div class="product-card__info">
+                                <div class="product-card__name">
+                                    <h6 style="overflow: hidden;text-overflow:ellipsis;">
+                                        <a href="{{ route('brands.products', $brand) }}" wire:navigate.hover title="{{$brand->name}}">{{ $brand->name }}</a>
+                                    </h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
+@endif
+
 @if(($show_option = setting('show_option'))->category_carousel ?? false)
 <div class="block block-products-carousel" data-layout="grid-cat">
     <div class="container">
@@ -254,58 +307,6 @@
 @endif
 <!-- .block-products-carousel / end -->
 @endforeach
-
-@if(($show_option = setting('show_option'))->brand_carousel ?? false)
-<div class="block block-products-carousel" data-layout="grid-cat">
-    <div class="container">
-        <div class="block-header">
-            <h3 class="block-header__title" style="padding: 0.375rem 1rem;">
-                <a href="{{ route('brands') }}" wire:navigate.hover>Brands</a>
-            </h3>
-            <div class="block-header__divider"></div>
-            <div class="block-header__arrows-list">
-                <button class="block-header__arrow block-header__arrow--left" type="button">
-                    <svg width="7px" height="11px">
-                        <use xlink:href="{{ asset('strokya/images/sprite.svg#arrow-rounded-left-7x11') }}"></use>
-                    </svg>
-                </button>
-                <button class="block-header__arrow block-header__arrow--right" type="button">
-                    <svg width="7px" height="11px">
-                        <use xlink:href="{{ asset('strokya/images/sprite.svg#arrow-rounded-right-7x11') }}"></use>
-                    </svg>
-                </button>
-            </div>
-        </div>
-        <div class="block-products-carousel__slider">
-            <div class="block-products-carousel__preloader"></div>
-            <div class="owl-carousel">
-                @foreach(brands()->chunk(1) as $brands)
-                <div>
-                    @foreach($brands as $brand)
-                    <div class="products-list__item">
-                        <div class="product-card">
-                            <div class="product-card__image">
-                                <a href="{{ route('brands.products', $brand) }}" wire:navigate.hover>
-                                    <img src="{{ cdn($brand->image_src, 100, 100) }}" alt="Product Image">
-                                </a>
-                            </div>
-                            <div class="product-card__info">
-                                <div class="product-card__name">
-                                    <h6 style="overflow: hidden;text-overflow:ellipsis;">
-                                        <a href="{{ route('brands.products', $brand) }}" wire:navigate.hover title="{{$brand->name}}">{{ $brand->name }}</a>
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    @endforeach
-                </div>
-                @endforeach
-            </div>
-        </div>
-    </div>
-</div>
-@endif
 
 @endsection
 
